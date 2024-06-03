c9b19fd340ac33f9ce2ec7fcb969b7ae158e9139
AJ Cooper <acooper@lcs.on.ca>
2024-06-03 12:57:33 -0400

    Initial Commit

---
+++ T&S/T_SApp.swift
@@ -0,0 +1,17 @@
+//
+//  T_SApp.swift
+//  T&S
+//
+//  Created by  Adam-James  Cooper on 2024-06-03.
+//
+
+import SwiftUI
+
+@main
+struct T_SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
