ea5a74cb4d74d7c3de519d663396e64650514e72
AJ Cooper <acooper@lcs.on.ca>
2024-06-09 14:06:20 -0400

    Added a helper file to identify our app to the Supabase servers.

---
+++ T&S/Helper/Supabase.swift
@@ -0,0 +1,8 @@
+//
+//  Supabase.swift
+//  T&S
+//
+//  Created by  Adam-James  Cooper on 2024-06-09.
+//
+
+import Foundation
---
+++ T&S/Views/SearchPage.swift
@@ -0,0 +1,18 @@
+//
+//  SearchPage.swift
+//  T&S
+//
+//  Created by  Adam-James  Cooper on 2024-06-09.
+//
+
+import SwiftUI
+
+struct SearchPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchPage()
+}
