21bddab705f09de745556566865659ec61076e47
AJ Cooper <acooper@lcs.on.ca>
2024-06-14 20:03:27 -0400

    I have run out of time to finish the app. I need to work on math exam. Changed the ItemDetailView so that you can see the sell name contact seller etc. Made the whole of AddNewItemView to help a user add a NewItem. Changed SearchItem many times. Search page works sorta!

---
+++ T&S/Views/AddNewItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewItemView.swift
+//  T&S
+//
+//  Created by  Adam-James  Cooper on 2024-06-13.
+//
+
+import SwiftUI
+
+struct AddNewItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewItemView()
+}
